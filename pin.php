befc4136ab16d17863b6396415d0396a3bdf994e
pitchatarn <pitchatarn@pitchatarns-MacBook-Air.local>
2014-08-17 21:28:40 +0700

    change ip target to localhost

--- pin.php
+++ pin.php
@@ -1,11 +1,20 @@
 <?php 
-  $url="http://54.179.16.196:3000/getPin?pinID=53eb7fe2954d008825a5a779";
+  $url="http://localhost:3000/getPin?pinID=53eb7fe2954d008825a5a779";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_URL, $url);
-  $result = curl_exec($ch);
+  $result = json_decode(curl_exec($ch), true);
 
   curl_close($ch);
-  var_dump(json_decode($result, true));
+  //var_dump($result);
 ?>
+
+<html>
+  <head>
+  </head>
+  <body>
+    <h3>It's not happinning... Yet</h3>
+    <?php echo $result["location"]["coordinates"][0]; ?>
+  </body>  
+</html>
