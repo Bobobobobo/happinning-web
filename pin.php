e47b34c9f415678270461e47bf7ffd42451737b0
root <root@ip-10-128-79-176.ap-southeast-1.compute.internal>
2014-08-17 14:38:28 +0000

    modify a bit in pin.php

--- pin.php
+++ pin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   $url="http://localhost:3000/getPin?pinID=53eb7fe2954d008825a5a779";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -16,5 +16,5 @@
   <body>
     <h3>It's not happinning... Yet</h3>
     <?php echo $result["location"]["coordinates"][0]; ?>
-  </body>  
+  </body>
 </html>
