d0d48fa825cb9d50f0c023f39b18c0a00504cb94
pitchatarn <pitchatarn@pitchatarns-MacBook-Air.local>
2014-08-27 21:53:36 +0700

    play with homepage

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
+  <body id="homepage">
     <div class="pin-page">
-      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
