4879cdc16d42387915be6f42c0077b70c83a9604
pitchatarn <pitchatarn@pitchatarns-MacBook-Air.local>
2014-08-27 21:54:45 +0700

    minor css fix

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
   </head>
   <body id="homepage">
     <div class="pin-page">
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
