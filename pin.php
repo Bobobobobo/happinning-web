8932623aa2c3384a76727155b588a54d47ab7a20
pitchatarn <pitchatarn@pitchatarns-MacBook-Air.local>
2014-08-29 02:37:21 +0700

    change title name

--- pin.php
+++ pin.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>What's happinning here</title>
+    <title>Happinning: What's Happinning here</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
