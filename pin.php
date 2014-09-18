6bde456a42c9ef8da038d3946f36c63942ec2c98
pitchatarn <pitchatarn@pitchatarns-MacBook-Air.local>
2014-09-18 14:19:09 +0700

    new pin page

--- pin.php
+++ pin.php
@@ -31,88 +31,105 @@
     <![endif]-->
   </head>
   <body>
-    <div id="pin-page">
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">What's Happinning?</a></li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
-              </div>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-              <li class="Profile">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img id="profile-image" src="http://identicon.org/?t=<?php echo $result['userId']?>&s=19"/>My Profile <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-              <li><a href="#"><i class="fa fa-power-off fa-lg" style="padding-right: 5px;"></i>Logout</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-      </nav>
-    </div>
     <div id="pin-block">
       <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <div class="panel-title">
-              <div id="pin-title">
-                <div class="row">
-                  <div class="col-md-9">
-                    <h4><?php echo $result['text']?></h4>
-                  </div>
-                  <div class="col-md-3">
-                    <h6 class="pull-right">by <img id="profile-image" src="http://identicon.org/?t=<?php echo $result['userId']?>&s=19"/> <a href="/user/<?php echo $result['userId']?>"><?php echo $result['username']?></a></h6>
-                  </div>
-                </div>
-              </div>
+        <div class="pin-panel">
+          <div class="pin-panel-heading">
+            <div id="pin-title">
+              <h4><?php echo $result['text']?></h4>
             </div>
           </div>
-          <div class="panel-body">
+          <!-- pin-panel-heading-->
+          <div class="pin-panel-body">
             <div class="row">
-              <div class="col-md-4">
-                  <div class="acf-map">
-                    <div class="marker" data-lat="<?php echo $result['location']['coordinates'][1]; ?>" data-lng="<?php echo $result['location']['coordinates'][0]; ?>"></div>
+              <div class="col-md-8">
+                  <div id="img-gallery">
+                    <img src="http://54.179.16.196:3000<?php echo $result['image']?>"/>
                   </div>
               </div>
-              <div class="col-md-8">
-                <div id="pin-detail" class="pull-right">
-                  <div id="local-block">
-                    At: <?php echo $result['location']['subLocality']?>, <?php echo $result['location']['locality']?>
+              <div class="col-md-4">
+                <div id="pin-detail">
+                  <div id="info-block">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <div class="icons">
+                          <i class="fa fa-map-marker fa-4x"></i>
+                        </div>
+                      </div>
+                      <div class="col-md-9">
+                        <div id="author-block">
+                          <h4><img id="profile-image" src="http://identicon.org/?t=<?php echo $result['userId']?>&s=19"/> <a href="/user/<?php echo $result['userId']?>"><?php echo $result['userId']?></a></h4>
+                        </div>
+                        <div id="local-block">
+                          At: <?php echo $result['location']['subLocality']?>, <?php echo $result['location']['locality']?>
+                        </div>
+                        <div id="date-block">
+                          <?php echo $result['uploadDate']?> mins ago
+                        </div>
+                      </div>
+                    </div>
+                    <div id="interaction-block">
+                      <div class="row">
+                        <div class="col-md-6 last">
+                            <i class="fa fa-heart-o fa-2x"></i><a class="int-text pull-right"><h4>Like</h4></a>
+                        </div>
+                        <div class="col-md-6">
+                            <i class="fa fa-pencil fa-2x"></i><a data-toggle="collapse" data-parent="#accordion" href="#collapseComment" class="int-text pull-right"><h4>Comment</h4></a>
+                        </div>
+                      </div>
+                    </div>
+                    <div id="collapseComment" class="panel-collapse collapse" style="padding-top: 10px;">
+                      <input type="text" class="form-control" placeholder="Comment here..."></input>
+                    </div>
                   </div>
-                  <div id="img-gallery">
-                    <img src="<?php echo $result['image']?>"/>
+                  <div id="comment-block">
+                    <div id="pin-title">
+                      TITLE
+                    </div>
+                    <div id="pin-comments">
+                      <div class="pin-comment">
+                        COMMENT 1
+                      </div>
+                      <div class="pin-comment">
+                        COMMENT 2
+                      </div>
+                      <div class="pin-comment">
+                        COMMENT 3
+                      </div>
+                      <div class="pin-comment">
+                        COMMENT 4
+                      </div>
+                      <div class="pin-comment">
+                        COMMENT 5
+                      </div>
+                    </div>
                   </div>
+                  <div id="map-block">
+                    <div class="map-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseMap">
+                        Maps <i class="fa fa-chevron-circle-down"></i>
+                      </a>
+                    </div>
+                    <div id="collapseMap" class="panel-collapse collapse">
+                      <?php if( !empty($result["location") ): ?>
+                        <div class="acf-map">
+                          <div class="marker" data-lat="<?php echo $result['location']['coordinates'][1]; ?>" data-lng="<?php echo $result['location']['coordinates'][2]; ?>"></div>
+                        </div>
+                      <?php endif; ?>
+                    </div>
+                  </div>
+                  <!-- map-block -->
                 </div>
+                <!-- pin-detail-->
               </div>
             </div>
           </div>
+          <!-- pin-panel-body -->
         </div>
+        <!-- pin-panel -->
       </div>
     </div>
+    <!-- pin-block -->
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
